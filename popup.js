71c8b557b97ffd0eee307686f302ef3e46267354
mishal23 <shahmishal1998@gmail.com>
2017-11-26 19:56:01 +0530

    basic code added

---
+++ popup.js
@@ -0,0 +1,41 @@
+var app=angular.module('notemaker',[]);
+
+app.controller('DisplayList',function($scope,$timeout) {
+
+    $scope.saved=localStorage.getItem('lists');
+    $scope.lists=(localStorage.getItem('lists')!==null)?JSON.parse($scope.saved):[{title:'First',done:'false'}];
+
+    /*
+    chrome.storage.sync.get(['title','done'], function (saved) {
+        $scope.lists = [{title:saved.title, done:saved.done}];
+    });
+    */
+    localStorage.setItem('lists',JSON.stringify($scope.lists));
+    //chrome.storage.sync.set({'title': JSON.stringify($scope.lists.title), 'done': JSON.stringify($scope.lists.done)});
+    $scope.add=function () {
+        if($scope.addTitle===undefined)
+        {
+
+        }
+        else {
+            $scope.lists.push({title: $scope.addTitle,done:false});
+            $scope.addTitle='';
+        }
+        //chrome.storage.sync.set({'title': JSON.stringify($scope.lists.title), 'done': JSON.stringify($scope.lists.done)});
+        localStorage.setItem('lists',JSON.stringify($scope.lists));
+
+    };
+    $scope.remove=function () {
+
+        var oldList=$scope.lists;
+        $scope.lists=[];
+        angular.forEach(oldList,function (todo){
+            if (!todo.done) {
+                $scope.lists.push(todo);
+            }
+        });
+
+        localStorage.setItem('lists',JSON.stringify($scope.lists));
+        //chrome.storage.sync.set({'title': JSON.stringify($scope.lists.title), 'done': JSON.stringify($scope.lists.done)});
+    };
+});
