25c080e0142a031cd5229129ea013f3fd50260aa
Shreyas Shankar <shreyas.shankar920@gmail.com>
2017-12-05 15:35:11 +0530

    date added

--- popup.js
+++ popup.js
@@ -23,21 +23,39 @@ app.controller('DisplayList',function($scope,$timeout) {
             $scope.lists.push({title: $scope.addTitle,done:false, date: d});
             $scope.addTitle='';
         }
+        add=document.getElementsByClassName('addbutton');
+        add[0].innerHTML='Add';
         //chrome.storage.sync.set({'title': JSON.stringify($scope.lists.title), 'done': JSON.stringify($scope.lists.done)});
         localStorage.setItem('lists',JSON.stringify($scope.lists));
 
     };
-    $scope.remove=function () {
+    $scope.edit=function () {
+        add=document.getElementsByClassName('addbutton');
+        add[0].innerHTML='Update';
+        this.list.done=true;
+        $scope.addTitle=$scope.remove().title;
+        document.getElementsByClassName('text')[0].focus();
 
+        
+        localStorage.setItem('lists',JSON.stringify($scope.lists));
+        //chrome.storage.sync.set({'title': JSON.stringify($scope.lists.title), 'done': JSON.stringify($scope.lists.done)});
+   
+    };
+    $scope.remove=function () {
+        var removedItem;
         var oldList=$scope.lists;
         $scope.lists=[];
         angular.forEach(oldList,function (todo){
             if (!todo.done) {
                 $scope.lists.push(todo);
             }
+            else{
+                removedItem=todo;
+            }
         });
 
         localStorage.setItem('lists',JSON.stringify($scope.lists));
         //chrome.storage.sync.set({'title': JSON.stringify($scope.lists.title), 'done': JSON.stringify($scope.lists.done)});
+        return removedItem;
     };
 });
