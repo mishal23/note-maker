154e2973b9541a37a9b1e466dccb378983cf3d9c
Shreyas Shankar <shreyas.shankar920@gmail.com>
2017-12-09 21:55:34 +0530

    title+notes

--- options.js
+++ options.js
@@ -1,10 +1,9 @@
 var app=angular.module('notemaker',[]);
 
-app.controller('DisplayList',function($scope,$timeout) {
+app.controller('DisplayList',function($scope,$filter,$timeout,$window) {
 
     $scope.saved=localStorage.getItem('lists');
     $scope.lists=(localStorage.getItem('lists')!==null)?JSON.parse($scope.saved):[{title:'First',done:'false'}];
-
     /*
     chrome.storage.sync.get(['title','done'], function (saved) {
         $scope.lists = [{title:saved.title, done:saved.done}];
@@ -19,21 +18,25 @@ app.controller('DisplayList',function($scope,$timeout) {
         }
         else {
             var d = new Date();
-            $scope.lists.push({title: $scope.addTitle,done:false, date:d});
+            $scope.lists.push({title: $scope.addTitle, notes: $scope.addNotes,done:false, date:JSON.stringify(d)});
             $scope.addTitle='';
+            $scope.addNotes='';
         }
         add=document.getElementsByClassName('addbutton');
         add[0].innerHTML='Add';
         //chrome.storage.sync.set({'title': JSON.stringify($scope.lists.title), 'done': JSON.stringify($scope.lists.done)});
         localStorage.setItem('lists',JSON.stringify($scope.lists));
+        $window.location.reload();
 
     };
     $scope.edit=function () {
         add=document.getElementsByClassName('addbutton');
         add[0].innerHTML='Update';
         this.list.done=true;
-        $scope.addTitle=$scope.remove().title;
-        document.getElementById('input').focus();
+        editItem=$scope.remove();
+        $scope.addTitle=editItem.title;
+        $scope.addNotes=editItem.notes;
+        document.getElementsByClassName('text')[1].focus();
 
 
         
@@ -58,4 +61,17 @@ app.controller('DisplayList',function($scope,$timeout) {
         //chrome.storage.sync.set({'title': JSON.stringify($scope.lists.title), 'done': JSON.stringify($scope.lists.done)});
         return removedItem;
     };
+});
+
+$(document).ready(function(){
+    $('.note').css({"display":"block"})
+    $('.notebutton').text("Hide");
+    $('.notebutton').click(function(){
+        note=$(this).parent().parent().next(".note");
+        note.toggle();
+        if (note.css("display")==="none")
+            $(this).text('Show');
+        else
+            $(this).text('Hide');
+    })
 });
--- popup.js
+++ popup.js
@@ -1,10 +1,9 @@
 var app=angular.module('notemaker',[]);
 
-app.controller('DisplayList',function($scope,$timeout) {
+app.controller('DisplayList',function($scope,$filter,$window,$timeout) {
 
     $scope.saved=localStorage.getItem('lists');
-    $scope.lists=(localStorage.getItem('lists')!==null)?JSON.parse($scope.saved):[{title:'First',done:'false',date: new Date()}];
-
+    $scope.lists=(localStorage.getItem('lists')!==null)?JSON.parse($scope.saved):[{title:'First',done:'false'}];
     /*
     chrome.storage.sync.get(['title','done'], function (saved) {
         $scope.lists = [{title:saved.title, done:saved.done}];
@@ -18,23 +17,28 @@ app.controller('DisplayList',function($scope,$timeout) {
 
         }
         else {
-            var d = new Date();
-            //dt = $filter('d')(new Date(), 'dd/MM/yyyy');
-            $scope.lists.push({title: $scope.addTitle,done:false, date: d});
-            $scope.addTitle='';
+        	var d = new Date();
+            $scope.lists.push({title: $scope.addTitle, notes: $scope.addNotes,done:false, date:JSON.stringify(d)});
+           	$scope.addTitle='';
+           	$scope.addNotes='';
         }
         add=document.getElementsByClassName('addbutton');
         add[0].innerHTML='Add';
         //chrome.storage.sync.set({'title': JSON.stringify($scope.lists.title), 'done': JSON.stringify($scope.lists.done)});
         localStorage.setItem('lists',JSON.stringify($scope.lists));
+        $window.location.reload();
 
     };
     $scope.edit=function () {
+    	$('.note').css({"display":"none"});
         add=document.getElementsByClassName('addbutton');
         add[0].innerHTML='Update';
         this.list.done=true;
-        $scope.addTitle=$scope.remove().title;
-        document.getElementsByClassName('text')[0].focus();
+        editItem=$scope.remove();
+        $scope.addTitle=editItem.title;
+        $scope.addNotes=editItem.notes;
+        document.getElementsByClassName('text')[1].focus();
+
 
         
         localStorage.setItem('lists',JSON.stringify($scope.lists));
@@ -58,4 +62,17 @@ app.controller('DisplayList',function($scope,$timeout) {
         //chrome.storage.sync.set({'title': JSON.stringify($scope.lists.title), 'done': JSON.stringify($scope.lists.done)});
         return removedItem;
     };
+});
+
+$(document).ready(function(){
+	$('.container').css({"width":"250px"});
+	$('.note').css({"width":"80%"})
+	$('.notebutton').click(function(){
+		note=$(this).parent().parent().next(".note");
+		note.toggle();
+		if (note.css("display")==="none")
+			$(this).text('Show');
+		else
+			$(this).text('Hide');
+	})
 });
