e2bfc1ca143a912b35c501328288817ebc24024a
Shreyas Shankar <shreyas.shankar920@gmail.com>
2017-12-12 01:47:30 +0530

    pagination+load function

--- options.js
+++ options.js
@@ -1,60 +1,10 @@
 $(document).ready(function(){
-    /*$($('.text')[0]).focus();*/
     $('#pagination-demo').twbsPagination({
             totalPages: 35,
             visiblePages: 4,
             onPageClick: function (event, page) {
-                pagelimit=2;
-                $('#page-content').text('Page '+page);
-
-                lists=JSON.parse(localStorage.lists);
-                lists.sort(function(a,b){
-                    return b.date>a.date;
-                });
-                pagelist=lists.slice((page-1)*pagelimit,page*pagelimit);
-                ol=$('.lists');
-                ol.empty();
-                li=[];
-                checkbox='<input type="checkbox"  class="checkbox">';
-                buttons='<ul class="notebuttons"><li><button type="button" class="editbutton">Edit</button></li><li><button type="button" class="notebutton">Show</button></li></ul>';
-                for(i in pagelist){
-                    li[i]='<li class="listItems">'+checkbox+'<span class="done check title">'+ pagelist[i].title +'</span>'+buttons+'<div class="note"><p class="text">'+ pagelist[i].notes +'</p></div>'+'</li> <br>';
-                }
-                ol.append(li);
-                $('.checkbox').click(function(){
-                    for (i in li)
-                        if ($($('.checkbox')[i]).prop("checked"))
-                            pagelist[i].done=true;
-                        else
-                            pagelist[i].done=false;
-                });
-
-                $('.addbutton').click(function(){
-                    remove();
-                    text=$('.text');
-                    if ($(text[0]).val()!=''){
-                        var d=new Date();
-                        lists.push({title: $(text[0]).val(), notes: $(text[1]).val(),done:false, date: d});
-                        $(text[0]).val('');
-                        $(text[1]).val('');
-                    }
-                    this.value="Add",
-                    localStorage.setItem('lists',JSON.stringify(lists));
-                    //location.reload();
-                    refresh();
-                });
-
-                var remove=function(){
-                    oldlist=lists;
-                    lists=[];
-                    for (i in oldlist){
-                        if(!oldlist[i].done)
-                            lists.push(oldlist[i]);
-                    localStorage.setItem('lists',JSON.stringify(lists));
-                    }
-                }
-
-                var refresh = function () {
+                var load = function (page) {                 //A function which loades the current page
+                    $($('.text')[0]).focus();
                     lists=JSON.parse(localStorage.lists);
                     lists.sort(function(a,b){
                         return b.date>a.date;
@@ -70,6 +20,14 @@ $(document).ready(function(){
                     }
                     ol.append(li);
 
+                    $('.checkbox').click(function(){
+                        for (i in li)
+                            if ($($('.checkbox')[i]).prop("checked"))
+                                lists[(page-1)*pagelimit+parseInt(i)].done=true;
+                            else
+                                lists[(page-1)*pagelimit+parseInt(i)].done=false;
+                    });
+
                     $('.note').css({"display":"block"})
                     $('.notebutton').text("Hide");
                     $('.notebutton').click(function(){
@@ -80,37 +38,59 @@ $(document).ready(function(){
                         else
                             $(this).text('Hide');
                     });
+
+                    $('#page-content').text('Page '+page);
+
+
+                    $('.addbutton').click(function(){               //function which adds a new entry
+                        remove();
+                        text=$('.text');
+                        if ($(text[0]).val()!=''){
+                            var d=new Date();
+                            lists.push({title: $(text[0]).val(), notes: $(text[1]).val(),done:false, date: d});
+                            $(text[0]).val('');
+                            $(text[1]).val('');
+                        }
+                        this.value="Add",
+                        localStorage.setItem('lists',JSON.stringify(lists));
+                        location.reload();
+                    });
+
+                    var remove=function(){                          //function which removes the checked items
+                        oldlist=lists;
+                        lists=[];
+                        for (i in oldlist){
+                            if(!oldlist[i].done)
+                                lists.push(oldlist[i]);
+                        localStorage.setItem('lists',JSON.stringify(lists));
+                        }
+                    }
+
+                    
+
+                    $('.removebutton').click(function(){            //function for removing
+                        remove();
+                        load(page);
+                    });
+
+                    $('.editbutton').click(function(){              //function for editing a specific entry
+                        edititem=$(this).parents('.listItems')[0];
+                        $($(edititem).children('.checkbox')[0]).prop('checked',true);
+                        indexp=$(edititem).index()/2;
+                        index=(page-1)*pagelimit + indexp;
+                        text=$('.text');
+                        lists[index].done=true;
+                        $(text[0]).val(lists[index].title);
+                        $(text[1]).val(lists[index].notes);
+                        $(text[1]).focus();
+                        $('.addbutton').attr('value','Update');
+                    });
                 }
 
-                $('.removebutton').click(function(){
-                    remove();
-                   // location.reload();
-                   refresh();
-                });
+                pagelimit=2;
 
-                $('.editbutton').click(function(){
-                    edititem=$(this).parents('.listItems')[0];
-                    $($(edititem).children('.checkbox')[0]).prop('checked',true);
-                    indexp=$(edititem).index();
-                    index=(page-1)*pagelimit + indexp;
-                    text=$('.text');
-                    lists[index].done=true;
-                    $(text[0]).val(lists[index].title);
-                    $(text[1]).val(lists[index].notes);
-                    $(text[1]).focus();
-                    $('.addbutton').value="Update";
-                });
+                load(page);
 
-                $('.note').css({"display":"block"})
-                $('.notebutton').text("Hide");
-                $('.notebutton').click(function(){
-                    note=$(this).parent().parent().next(".note");
-                    note.toggle();
-                    if (note.css("display")==="none")
-                        $(this).text('Show');
-                    else
-                        $(this).text('Hide');
-                });
-            }
+           }
 });
 });
--- popup.js
+++ popup.js
@@ -1,79 +1,97 @@
 $(document).ready(function(){
-    $($('.text')[0]).focus();
-    
-    lists=[];
-    if(localStorage.lists!=undefined)
-        lists=JSON.parse(localStorage.lists);
-    
-    lists.sort(function(a,b){
-        return b.date>a.date;
-    });
-    ol=$('.lists');
-    li=[];
-    checkbox='<input type="checkbox"  class="checkbox">';
-    buttons='<ul class="notebuttons"><li><button type="button" class="editbutton">Edit</button></li><li><button type="button" class="notebutton">Show</button></li></ul>';
-    for(i in lists){
-        li[i]='<li class="listItems">'+checkbox+'<span class="done check title">'+ lists[i].title +'</span>'+buttons+'<div class="note"><p class="text">'+ lists[i].notes +'</p></div>'+'</li> <br>';
-    }
-    ol.append(li);
-    $('.checkbox').click(function(){
-        for (i in li)
-            if ($($('.checkbox')[i]).prop("checked"))
-                lists[i].done=true;
-    });
+    $('#pagination-demo').twbsPagination({
+            totalPages: 35,
+            visiblePages: 4,
+            onPageClick: function (event, page) {
+                var refresh = function (page) {                 //A function which refreshes the current page
+                    $($('.text')[0]).focus();
+                    lists=JSON.parse(localStorage.lists);
+                    lists.sort(function(a,b){
+                        return b.date>a.date;
+                    });
+                    pagelist=lists.slice((page-1)*pagelimit,page*pagelimit);
+                    ol=$('.lists');
+                    ol.empty();
+                    li=[];
+                    checkbox='<input type="checkbox"  class="checkbox">';
+                    buttons='<ul class="notebuttons"><li><button type="button" class="editbutton">Edit</button></li><li><button type="button" class="notebutton">Show</button></li></ul>';
+                    for(i in pagelist){
+                        li[i]='<li class="listItems">'+checkbox+'<span class="done check title">'+ pagelist[i].title +'</span>'+buttons+'<div class="note"><p class="text">'+ pagelist[i].notes +'</p></div>'+'</li> <br>';
+                    }
+                    ol.append(li);
 
-    $('.addbutton').click(function(){
-        remove();
-        text=$('.text');
-        if ($(text[0]).val()!=''){
-            var d=new Date();
-            lists.push({title: $(text[0]).val(), notes: $(text[1]).val(),done:false, date: d});
-            $(text[0]).val('');
-            $(text[1]).val('');
-        }
-        this.value="Add",
-        localStorage.setItem('lists',JSON.stringify(lists));
-        location.reload();
-    });
+                    $('.checkbox').click(function(){
+                        for (i in li)
+                            if ($($('.checkbox')[i]).prop("checked"))
+                                lists[(page-1)*pagelimit+parseInt(i)].done=true;
+                            else
+                                lists[(page-1)*pagelimit+parseInt(i)].done=false;
+                    });
 
-    var remove=function(){
-        oldlist=lists;
-        lists=[];
-        for (i in oldlist){
-            if(!oldlist[i].done)
-                lists.push(oldlist[i]);
-            else
-                removedItem=oldlist[i];
-        localStorage.setItem('lists',JSON.stringify(lists));
-        }
-    }
+                    $('.container').css({"width":"250px"});
+                    $('.note').css({"width":"80%"})
+                    $('.notebutton').click(function(){
+                        note=$(this).parent().parent().next(".note");
+                        note.toggle();
+                        if (note.css("display")==="none")
+                            $(this).text('Show');
+                        else
+                            $(this).text('Hide');
+                    });
 
-    $('.removebutton').click(function(){
-        remove();
-        location.reload();
-    });
+                    $('#page-content').text('Page '+page);
 
-    $('.editbutton').click(function(){
-        edititem=$(this).parents('.listItems')[0];
-        $($(edititem).children('.checkbox')[0]).prop('checked',true);
-        index=$(edititem).index();
-        text=$('.text');
-        lists[index].done=true;
-        $(text[0]).val(lists[index].title);
-        $(text[1]).val(lists[index].notes);
-        $(text[1]).focus();
-        $('.addbutton').value="Update";
-    });
+                    $('.addbutton').click(function(){               //function which adds a new entry
+                        remove();
+                        text=$('.text');
+                        if ($(text[0]).val()!=''){
+                            var d=new Date();
+                            lists.push({title: $(text[0]).val(), notes: $(text[1]).val(),done:false, date: d});
+                            $(text[0]).val('');
+                            $(text[1]).val('');
+                        }
+                        this.value="Add",
+                        localStorage.setItem('lists',JSON.stringify(lists));
+                        location.reload();
+                    });
 
-   
-    $('.container').css({"width":"250px"});
-    $('.note').css({"width":"80%"})
-    $('.notebutton').click(function(){
-        note=$(this).parent().parent().next(".note");
-        note.toggle();
-        if (note.css("display")==="none")
-            $(this).text('Show');
-        else
-            $(this).text('Hide');
-    })
+                    var remove=function(){                          //function which removes the checked items
+                        oldlist=lists;
+                        lists=[];
+                        for (i in oldlist){
+                            if(!oldlist[i].done)
+                                lists.push(oldlist[i]);
+                        localStorage.setItem('lists',JSON.stringify(lists));
+                        }
+                    }
+
+                    
+
+                    $('.removebutton').click(function(){            //function for removing
+                        remove();
+                        refresh(page);
+                    });
+
+                    $('.editbutton').click(function(){              //function for editing a specific entry
+                        edititem=$(this).parents('.listItems')[0];
+                        $($(edititem).children('.checkbox')[0]).prop('checked',true);
+                        indexp=$(edititem).index()/2;
+                        index=(page-1)*pagelimit + indexp;
+                        text=$('.text');
+                        lists[index].done=true;
+                        $(text[0]).val(lists[index].title);
+                        $(text[1]).val(lists[index].notes);
+                        $(text[1]).focus();
+                        $('.addbutton').attr('value','Update');
+                    });
+                }
+
+
+                pagelimit=2;
+
+                refresh(page);
+
+                
+            }
+});
 });
