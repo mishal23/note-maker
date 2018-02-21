d6cab43654b03da3b7a9d8baaf0499dcec3c9b82
mehnazyunus <mehnaz138@gmail.com>
2018-02-22 00:06:55 +0530

    AJAX calls added

--- server/models/notes.js
+++ server/models/notes.js
@@ -7,7 +7,8 @@ var noteSchema = new Schema ( {
     title: {type: String, required: true},
     content: {type: String, required: true},
     createdBy : {type: mongoose.Schema.Types.ObjectId, ref:'User'},
-    date : {type: Date, required: true}
+    date : {type: Date, required: true},
+    done: {type: Boolean, required : true}
 });
 
 var Note = mongoose.model('Note', noteSchema);
