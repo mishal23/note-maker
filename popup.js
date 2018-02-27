eec65eb1e9f26fa624ef05ec9e892a936f03b42a
Shreyas Shankar <shreyas.shankar920@gmail.com>
2018-02-28 01:31:59 +0530

    pagination and refresh fixed

--- popup.js
+++ popup.js
@@ -4,25 +4,20 @@ $(document).ready(function(){
     chrome.storage.sync.get("loggedIn",function(data){
         if(data.loggedIn===undefined)
         {
-            $('.signup').hide();
-            $('.valid').hide();
             $('.login').show();
-            $('.main_part').hide();
+            $('.login').siblings().hide();
+            $('.valid').hide();
         }
         else
         {
-            $('.signup').hide();
-            $('.valid').hide();
-            $('.login').hide();
-            $('.main_part').show();           
+            $('.main_part').show();
+            $('.main_part').siblings().hide();
         }
     });
 
     $('.link_signup').click(function(){
-        $('.login').hide();
-        $('.main_part').hide();
-        $('.valid').hide();
         $('.signup').show();
+        $('.signup').siblings().hide();
     });
 
     $('.loginbtn').click(function(){
@@ -35,15 +30,14 @@ $(document).ready(function(){
             var username = $('.login_username').val();
             var password = $('.login_password').val();
             $.post("http://notemaker-server.herokuapp.com/users/" + username,{password:password}, function(data){
-                console.log(data);
                 if(data.statusText === "Success")
                 {   
                     chrome.storage.sync.set({"loggedIn": data.user_id}, function(){
-                        console.log("Set hua?" + data.loggedIn);
                     });
-                    $('.main_part').show();
-                    $('.login').hide();
-                    $('.valid').hide();
+                    location.reload();
+                }
+                else{
+                    $('.valid').show();
                 }
             });
         }
@@ -85,6 +79,7 @@ $(document).ready(function(){
             if(data.status==='User created'){
                 $('.signup').hide();
                 $('.main_part').show();
+
             }
         })
 
@@ -106,44 +101,57 @@ $(document).ready(function(){
         
     });
 
+
+    //A function which refreshes the current page
+
+
+
+
     $('#pagination-demo').twbsPagination({
 
-            totalPages: 35,
-            visiblePages: 4,
-            onPageClick: function (event, page) {
+        totalPages: 35,
+        visiblePages: 4,
+        onPageClick: function (event, page) {
 
-                var refresh = function (page) {  //A function which refreshes the current page
+            var pagelimit = 2;
+            var edit=false;
+            var note_edit_id;
 
-                    $($('.text')[0]).focus();
-                    chrome.storage.sync.get("loggedIn",function(data){
+            var refresh = function (page) {  //A function which refreshes the current page
+                $('#page-content').text('Page ' + page);
+                $('#title').val('');
+                $('#content').val('');
+                $('#title').focus();
+
+                chrome.storage.sync.get("loggedIn",function(data){
 
                     $.get("http://notemaker-server.herokuapp.com/notes/" + data.loggedIn, function(lists){
-
-                        lists.sort(function(a,b){
-                            return b.date > a.date;
-                        });
-
+                        lists.reverse();
                         var pagelist = lists.slice((page-1)*pagelimit, page*pagelimit);
-                        var ol = $('.lists');
-                        ol.empty();
+                        var display_list = $('.lists');
+                        display_list.empty();
                         var li = [];
                         checkbox = '<input type="checkbox"  class="checkbox">';
+
+                        // Button variable to add Edit/Show button
                         buttons =
                             '<ul class="notebuttons">' +
-                                '<li><button type="button" class="editbutton">Edit</button></li>' +
-                                '<li><button type="button" class="notebutton">Show</button></li>' +
+                            '<li><button type="button" class="editbutton">Edit</button></li>' +
+                            '<li><button type="button" class="notebutton">Show</button></li>' +
                             '</ul>';
+
+                        // notes on that page
                         for(i in pagelist){
                             li[i] =
                                 '<li class="listItems">'+checkbox+'' +
-                                    '<span class="done check title">'+ pagelist[i].title +'</span>'
-                                    +buttons+
-                                    '<div class="note">' +
-                                        '<p class="text">'+ pagelist[i].content +'</p>' +
-                                    '</div>'+
+                                '<span class="done check title">'+ pagelist[i].title +'</span>'
+                                +buttons+
+                                '<div class="note">' +
+                                '<p class="text">'+ pagelist[i].content +'</p>' +
+                                '</div>'+
                                 '</li> <br>';
                         }
-                        ol.append(li);
+                        display_list.append(li);
 
                         $('.checkbox').click(function(){
                             for (i in li)
@@ -151,10 +159,9 @@ $(document).ready(function(){
                                     lists[(page-1)*pagelimit+parseInt(i)].done = true;
                                 else
                                     lists[(page-1)*pagelimit+parseInt(i)].done = false;
+
                         });
 
-                        $('.container').css({"width":"250px"});
-                        $('.note').css({"width":"80%"});
 
                         $('.notebutton').click(function(){
                             var note = $(this).parent().parent().next(".note");
@@ -165,95 +172,95 @@ $(document).ready(function(){
                                 $(this).text('Hide');
                         });
 
-                        $('#page-content').text('Page ' + page);
-
-                        $('.addbutton').click(function(){               //function which adds a new entry
-                            //remove();
-                            //console.log("came in add");
-                            // chrome.storage.sync.get("loggedIn",function(data){
-                                text=$('.text');
-                                if ($(text[0]).val()!==''){
-                                    var d = new Date();
-                                    var newNote = {
-                                        title: $(text[0]).val(),
-                                        content: $(text[1]).val(),
-                                        createdBy : data.loggedIn,
-                                        date: d,
-                                        done:false
-                                    };
-                                    $.post("http://notemaker-server.herokuapp.com/notes", newNote, function() {
-                                        refresh(page);
-                                    });
-
-                                    $(text[0]).val('');
-                                    $(text[1]).val('');
-                                }
-                            // });
-                            this.value = "Add";
+                        $('.editbutton').click(function(){              //function for editing a specific entry
+                            var edititem = $(this).parents('.listItems')[0];
+                            // $($(edititem).children('.checkbox')[0]).prop('checked',true);
+                            var indexp = $(edititem).index()/2;
+                            index = (page-1)*pagelimit + indexp;
+                            lists[index].done = true;
+                            note_edit_id=lists[index]._id;
+                            edit=true;
+                            $('#title').val(lists[index].title);
+                            $('#content').val(lists[index].content);
+                            $('#content').focus();
+                            $("#addbutton").attr('value','Update');
                         });
 
-                        var remove = function(){        //function which removes the checked items
+                        remove = function(){        //function which removes the checked items
                             for(i in lists) {
                                 if(lists[i].done) {
                                     $.ajax ({
-                                       type: 'DELETE',
-                                       url: "http://notemaker-server.herokuapp.com/notes/"+lists[i]._id
+                                        type: 'DELETE',
+                                        url: "http://notemaker-server.herokuapp.com/notes/"+lists[i]._id
+                                    }).done(function(){
+                                        refresh(page);
                                     });
                                 }
                             }
 
                         };
 
-                        $('.removebutton').click(function(){            //function for removing
-                            remove();
-                            refresh(page);
-                        });
-
-                        $('.editbutton').click(function(){              //function for editing a specific entry
-                            var edititem = $(this).parents('.listItems')[0];
-                            $($(edititem).children('.checkbox')[0]).prop('checked',true);
-                            var indexp = $(edititem).index()/2,
-                            index = (page-1)*pagelimit + indexp,
-                            text = $('.text');
-                            lists[index].done = true;
-                            $(text[0]).val(lists[index].title);
-                            $(text[1]).val(lists[index].content);
-                            $(text[1]).focus();
-                            $(".addbutton").attr('value','Update');
-                            $('.addbutton').addClass('updating');
-                        });
-
-                        $('.addbutton .updating').click(function() {
-                            console.log("Entered updating");
-                            var text = $('.text');
-                            for(i in lists) {
-                                if(lists[i].done) {
-                                    $.ajax({
-                                        type: 'PUT',
-                                        url: 'http://notemaker-server.herokuapp.com/notes/'+lists[i]._id,
-                                        dataType: 'json',
-                                        data: {
-                                            title: $(text[0]).val(),
-                                            content: $(text[1]).val(),
-                                            createdBy : lists[i].createdBy,
-                                            date: lists[i].data,
-                                            done:false
-                                        }
-                                    });
-                                }
-                            }
-                            $('.addbutton .updating').removeClass('updating');
-                        });
-
                     });
                 });
 
-                };
+            };
 
-                var pagelimit = 2;
+            refresh(page);
 
-                refresh(page);
-            }
+
+            chrome.storage.sync.get("loggedIn",function(data) {
+
+                $('#addbutton').click(function () {               //function which adds a new entry
+                    this.value = "Add";
+                    if ($('#title').val() !== '') {
+                        $('#no_tittle').text("");
+
+                        var d = new Date();
+                        var newNote = {
+                            title: $('#title').val(),
+                            content: $('#content').val(),
+                            createdBy: data.loggedIn,
+                            date: d,
+                            done: false
+                        };
+
+                        if (edit) {
+                            $.ajax({
+                                type: 'PUT',
+                                url: 'http://notemaker-server.herokuapp.com/notes/' + note_edit_id,
+                                dataType: 'json',
+                                data: newNote
+                            }).done(function(){
+                                edit=false;
+                                refresh(1);
+                            });
+                        }
+                        else {
+                            $.post("http://notemaker-server.herokuapp.com/notes", newNote, function () {
+                                edit=false;
+                                refresh(1);
+                            });
+                        }
+
+                    }
+                    else{
+                        $('#no_tittle').text("Please Add a title!");
+                    }
+
+                });
+
+
+
+                $('#removebutton').click(function(){            //function for removing
+                    remove();
+                });
+            });
+
+        }
     });
+
+
+
+
 
 });
