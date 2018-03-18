1218f8802eb000e0fe6001f8701f1b355feeb9b9
Mishal Shah <shahmishal1998@gmail.com>
2018-03-18 21:46:10 +0530

    Removed unnecessary files

--- script.js
+++
@@ -1,5 +0,0 @@
-//document.body.style.backgroundColor = "black";
-var button = document.createElement("button");
-var text = document.createTextNode("Add to NoteMaker");
-button.appendChild(text);
-// document.body.appendChild(button);
